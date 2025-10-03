659b2948c1f597678138f3a03b2f98e19cbddb6b
Alexey Yakovlev <alexey.yakovlev.05@mail.ru>
2025-10-03 13:17:18 +0500

    add solve

--- src/main/java/ru/naumen/collection/task2/Task2.java
+++ src/main/java/ru/naumen/collection/task2/Task2.java
@@ -1,7 +1,6 @@
 package ru.naumen.collection.task2;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /**
  * Дано:
@@ -35,7 +34,32 @@ public class Task2 {
      * Возвращает дубликаты пользователей, которые есть в обеих коллекциях
      */
     public static List<User> findDuplicates(Collection<User> collA, Collection<User> collB) {
-        // TODO реализовать метод
-        return null;
+        Set<User> setB = new HashSet<>(collB);
+
+        List<User> intersection = new LinkedList<>();
+        for (User user : collA) {
+            if (setB.contains(user)) {
+                intersection.add(user);
+            }
+        }
+
+        return intersection;
     }
 }
+
+/**
+ * ОБОСНОВАНИЕ:
+ *
+ * 1. Одну коллекцию конвертирую в HashSet, другую оставляю как есть. Результат - intersection в LinkedList
+ *
+ * 2. Сложность - O(n). Перебираем все элементы в множестве.
+ *
+ * 3.
+ *      1 - Добавление в конец intersection - O(1)
+ *      2 - Проверка наличия элемента в множестве - O(1)
+ *      3 - HashMap может не выдавать O(1), если количество элементов в разы больше чем бакетов.
+ *      Но мы в данном случае создаем его через конструктор, а там вместимость уже рассчитывается.
+ *      4 - Итерация по коллекции - O(n). Мы итерируемся не по set,
+ *      потому что set может при итерации не выдавать O(1), если большая разряженность между хэш кодами ключей.
+ *
+ */
